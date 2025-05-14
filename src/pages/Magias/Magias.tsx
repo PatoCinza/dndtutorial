7cdfac1f01e440f9fbbaf60968a7b4f1d3dd67e1
Gabriel Luis Da Silva <cinza@MacBook-Pro-de-Gabriel.local>
2025-05-14 00:03:02 -0300

    refactor: improve spell filtering and menu layout

--- src/pages/Magias/Magias.tsx
+++ src/pages/Magias/Magias.tsx
@@ -12,6 +12,32 @@ import { SpellCard } from '../../components/SpellCard/SpellCard';
 function ListaMagias() {
     const [spellList, setSpellList] = useState(spells);
 
+    const filterSpellList = (key: string, query: string) => {
+        if (query === 'all') {
+            setSpellList(spells);
+        } else {
+            setSpellList(spells.filter((spell) => {
+                if(typeof (spell as any)[key] === 'number') {
+                    return (spell as any)[key] === Number(query);
+                } else {
+                    return (spell as any)[key].includes(query);
+                }
+            }));
+        }
+    }
+
+    const filterSpellListByClass = (className: string) => {
+        filterSpellList('classes', className);
+    }
+
+    const filterSpellListByLevel = (level: string) => {
+        filterSpellList('level', level);
+    }
+
+    const filterSpellListBySchool = (school: string) => {
+        filterSpellList('school', school);
+    }
+
     return (
         <div className="App">
             Referencia (cópia pra ser honesto): <a
@@ -24,9 +50,9 @@ function ListaMagias() {
                 <div id="menu">
                     <div className="menu-item">
                         <p>Classe</p>
-                        <div></div>
-                        <select id="class-select">
-                            <option value="all">-----</option>
+                        <select id="class-select" className="modern-select"
+                            onChange={(e) => filterSpellListByClass(e.target.value)}>
+                            <option value="all">Selecione uma classe</option>
                             <option value="bardo">Bardo</option>
                             <option value="bruxo">Bruxo</option>
                             <option value="clérigo">Clérigo</option>
@@ -37,8 +63,42 @@ function ListaMagias() {
                             <option value="patrulheiro">Patrulheiro</option>
                         </select>
                     </div>
-                    <div className="menu-space">
+                    <div className="menu-space" />
+                    <div className="menu-item">
+                        <p>Nível</p>
+                        <select id="level-select" className="modern-select"
+                            onChange={(e) => filterSpellListByLevel(e.target.value)}>
+                            <option value="all">Todos os níveis</option>
+                            <option value="0">Truque</option>
+                            <option value="1">1º nível</option>
+                            <option value="2">2º nível</option>
+                            <option value="3">3º nível</option>
+                            <option value="4">4º nível</option>
+                            <option value="5">5º nível</option>
+                            <option value="6">6º nível</option>
+                            <option value="7">7º nível</option>
+                            <option value="8">8º nível</option>
+                            <option value="9">9º nível</option>
+                        </select>
                     </div>
+                    <div className="menu-space" />
+                    <div className="menu-item">
+                        <p>Escola</p>
+                        <select id="school-select" className="modern-select"
+                            onChange={(e) => filterSpellListBySchool(e.target.value)}>
+                            <option value="all">Todas as escolas</option>
+                            <option value="abjuração">Abjuração</option>
+                            <option value="adivinhação">Adivinhação</option>
+                            <option value="conjuração">Conjuração</option>
+                            <option value="encantamento">Encantamento</option>
+                            <option value="evocação">Evocação</option>
+                            <option value="ilusão">Ilusão</option>
+                            <option value="necromancia">Necromancia</option>
+                            <option value="transmutação">Transmutação</option>
+                        </select>
+                    </div>
+                    <div className="menu-space" />
+
                     <div id="show-known" className="menu-item">
                         <p>Conhecidas <UnknownSpell /></p>
                     </div>
